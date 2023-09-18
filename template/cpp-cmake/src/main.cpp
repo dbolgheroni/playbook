12a26542a4b1b862f656fcafec183bbed67d418c
Daniel Bolgheroni <dbolgheroni@protonmail.com>
2023-09-17 23:33:33 -0300

    Add cpp-cmake template

---
+++ template/cpp-cmake/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "pb.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    pb();
+
+    return 0;
+}
---
+++ template/cpp-cmake/src/pb.cpp
@@ -0,0 +1,8 @@
+#include "pb.h"
+#include <iostream>
+
+using namespace std;
+
+int pb(void) {
+    return 0;
+}
